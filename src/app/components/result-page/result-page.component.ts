576ffff1ac73f3bb4f8d5cc5ca315ea1f9ecda35
firdosetaj <61687965+firdosetaj@users.noreply.github.com>
2020-03-31 12:06:49 +0530

    Update result-page.component.ts
    added score

--- src/app/components/result-page/result-page.component.ts
+++ src/app/components/result-page/result-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
 import { Question } from './../../model/Questions';
 import { QuizService } from './../../components/quiz/quiz.service';
 
@@ -11,14 +12,49 @@ export class ResultPageComponent implements OnInit {
   questions:any = [];
   totalNumberofQuestions: number = 0;
   correctQuestions: number = 0;
+  userName ="";
+  quizNumber =0;
+  
 
-  constructor(private quizService: QuizService) { }
+  constructor(
+    private router: Router,
+    private ngZone: NgZone,
+    private quizService: QuizService) {
+    this.userName = this.router.getCurrentNavigation().extras.state.username;
+    this.quizNumber = this.router.getCurrentNavigation().extras.state.quizNumber;
+  }
+  
 
   ngOnInit(): void {
     this.quizService.getQuizQuestions().subscribe(res => {
           this.questions = res;
           this.totalNumberofQuestions = this.questions.length;
     });
+
+  var questionBank;
+  this.quizService.getQuizQuestions().subscribe(
+	(res) => {
+		questionBank=res;
+	}, (error) => {
+	  console.log(error);
+	});
+	
+ 
+  this.quizService.getUserAnswers(this.userName,this.quizNumber).subscribe(
+	(res) => {
+		  console.log('Quiz score goes herettttt!',questionBank);
+		  //questionBank.forEach(function(element){console.log('element her',element)})
+		  for(var i=0;i<questionBank.length;i++){			  
+			  for(var answerData=0;answerData<res.length;answerData++){
+				  if((questionBank[i]["questionID"]==res[answerData]["questionID"]) && (questionBank[i]["answerID"]==res[answerData]["userAnswerID"])){
+					  this.correctQuestions++;		
+				}
+			  }
+		  }
+		  console.log("result goes here",this.correctQuestions)
+		}, (error) => {
+		  console.log(error);
+		});
 
   }
 
