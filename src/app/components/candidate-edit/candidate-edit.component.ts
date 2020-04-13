855ecd9a91992b0cf6a67c56dae03a244b6c0102
Sherin Joseph <sherijos@in.ibm.com>
2020-04-13 23:32:51 +0530

    Update a Document in Candidate Collection

--- src/app/components/candidate-edit/candidate-edit.component.ts
+++ src/app/components/candidate-edit/candidate-edit.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { ApiService } from './../../service/api.service';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { DatePipe } from '@angular/common';
+
 
 
 @Component({
@@ -14,28 +16,32 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 export class CandidateEditComponent implements OnInit {
   submitted = false;
   editForm: FormGroup;
-  candidateData: Candidate[];
   EmployeeProfile:any = ['Associate Developer', 'Senior Developer', 'Technical Lead', 'Associate Architect', 'Architect','Test Analyst','Test Manager', 'Project Manager']
   Band:any = [];
+  candidate : Candidate;
+  //adminUsername : String = "";
 
   constructor(
     public fb: FormBuilder,
     private actRoute: ActivatedRoute,
     private apiService: ApiService,
-    private router: Router
+    private router: Router,
+    private datePipe: DatePipe
   ) {}
 
   ngOnInit() {
     this.readBand();
     this.updateCandidate();
     let id = this.actRoute.snapshot.paramMap.get('id');
+    //this.adminUsername = this.actRoute.snapshot.paramMap.get('adminUsername');
     this.getCandidate(id);
     this.editForm = this.fb.group({
       employeeName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       band: ['', [Validators.required]],
       JRSS: ['', [Validators.required]],
-      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      dateOfJoining: ['', [Validators.required]]
     })
   }
 
@@ -68,11 +74,16 @@ export class CandidateEditComponent implements OnInit {
     this.apiService.getCandidate(id).subscribe(data => {
       this.editForm.setValue({
         employeeName: data['employeeName'],
-        email: data['username'],
+        email: data['email'],
         band: data['band'],
         JRSS: data['JRSS'],
         phoneNumber: data['phoneNumber'],
+        dateOfJoining : this.datePipe.transform(data['dateOfJoining'], 'yyyy-MM-dd')
       });
+      this.candidate = new Candidate(data['employeeName'],
+      data['email'], data['band'], data['JRSS'], data[ 'phoneNumber'], data['dateOfJoining'],
+      data['createdBy'], data['createdDate'], data['updatedBy'], data['updatedDate'],
+      data['username'])
     });
   }
 
@@ -82,24 +93,38 @@ export class CandidateEditComponent implements OnInit {
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
       band: ['', [Validators.required]],
       JRSS: ['', [Validators.required]],
-      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      dateOfJoining: ['', [Validators.required]]
     })
   }
 
   onSubmit() {
     this.submitted = true;
+    let updatedCandidate = new Candidate(this.editForm.value.employeeName,
+      this.editForm.value.email,
+      this.editForm.value.band,
+      this.editForm.value.JRSS,
+      this.editForm.value.phoneNumber,
+      this.editForm.value.dateOfJoining,
+      this.candidate.createdBy,
+      this.candidate.createdDate,
+      this.candidate.updatedBy,
+      //this.adminUsername,
+      new Date(),
+      this.editForm.value.email
+      );
     if (!this.editForm.valid) {
       return false;
     } else {
       if (window.confirm('Are you sure?')) {
         let id = this.actRoute.snapshot.paramMap.get('id');
-        this.apiService.updateCandidate(id, this.editForm.value)
+        this.apiService.updateCandidate(id, updatedCandidate)
           .subscribe(res => {
             this.router.navigateByUrl('/candidates-list');
             console.log('Content updated successfully!')
           }, (error) => {
             console.log(error)
-          })
+          })  
       }
     }
   }
