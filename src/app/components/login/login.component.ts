e1aee7cc12a608cb6800405f3d9dcc3f90740a67
Harshitha Edpuganti <edharshi@in.ibm.com>
2020-04-13 12:54:54 +0530

    change password

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -95,7 +95,7 @@ export class LoginComponent implements OnInit {
              if (res.accessLevel === 'admin') {
                this.ngZone.run(() => this.router.navigateByUrl('/create-candidate'))
              } else {
-              if(res.quizNumber == 1){
+              if(res.quizNumber == 1 && res.status == 'Active'){
                 this.ngZone.run(() => this.router.navigateByUrl('/change-password',{state:{username:res.username,quizNumber:res.quizNumber}}))  
                }else{
                 if(res.status == 'Active'){
