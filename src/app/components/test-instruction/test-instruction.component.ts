9b5b1ae6dab813e4763bfa31f8a110b2820b16d7
Adharsh <adharsh2208@gmail.com>
2020-04-23 10:28:21 +0530

    Updating user status in instruction page

--- src/app/components/test-instruction/test-instruction.component.ts
+++ src/app/components/test-instruction/test-instruction.component.ts
@@ -2,6 +2,7 @@ import { Router, ActivatedRoute , NavigationExtras } from '@angular/router';
 import { Component, OnInit, NgZone } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { appConfig } from './../../model/appConfig';
+import { ApiService } from './../../service/api.service';
 
 @Component({
   selector: 'app-test-instruction',
@@ -13,12 +14,14 @@ export class TestInstructionComponent implements OnInit {
   numOfMins;
   loading = false;
   submitted = false;
+  status;
   example;
   username;
   quizNumber;
  constructor(
       public fb: FormBuilder,
       private router: Router,
+      private apiService: ApiService,
       private ngZone: NgZone,
 
     ) {
@@ -37,11 +40,14 @@ export class TestInstructionComponent implements OnInit {
       this.submitted = true;
       console.log("Form Submitted!");
       // stop here if form is invalid
-      this.loading = true;
-      this.ngZone.run(() => this.router.navigateByUrl('/take-quiz',{state:{username:this.username,quizNumber:this.quizNumber}}));
-                
-      }
-
- 
- 
-}
+      this.loading = true;      
+      this.status = "Inactive";  
+	    // Update status column in Users table                     
+      this.apiService.updateUsersStatus(this.username,this.status,this.username).subscribe(
+      (res) => {
+              console.log(this.username+'Status column updated successfully in Users table');                 
+               }, (error) => {                
+              console.log("Error found while updating status column of Users table - " + error);
+               });
+              this.ngZone.run(() => this.router.navigateByUrl('/take-quiz',{state:{username:this.username,quizNumber:this.quizNumber}}));
+               }}
